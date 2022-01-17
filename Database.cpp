a752413c5557e5c94a7e691e2d4e9331099bd064
Suprayvo <68844142+supravo474@users.noreply.github.com>
2022-01-17 23:43:09 +0530

    Update Database.cpp

--- Database.cpp
+++ Database.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    // create your dictonary
+    
     map<string, string>dict = { {"Harry", "TreatPeopleWith"} };
     string username, password;
     cout<<"Enter username: "<<endl;
