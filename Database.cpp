60b9345b154f5d274d42007c6584e063f505d63b
Suprayvo <68844142+supravo474@users.noreply.github.com>
2022-01-17 23:55:04 +0530

    Update Database.cpp

--- Database.cpp
+++ Database.cpp
@@ -4,14 +4,23 @@
 using namespace std;
 
 int main() {
+    // create your dictonary
+    map<string, string>dict = { 
+    {"Harry", "TreatPeopleWith"}, 
+    {"Zain", "ICanDoThisForever"}, 
+    {"Liam", "Remember"}, 
+    {"Niall", "NiceToMeetYa"}, 
+    {"Louis", "Always_YOU"}
     
-    map<string, string>dict = { {"Harry", "TreatPeopleWith"} };
+    };
+
+
     string username, password;
     cout<<"Enter username: "<<endl;
     cin>>username;
     cout<<"Enter password: "<<endl;
     cin>>password;
-    // Check if provided username and password matches with the one in the database
+    // Check if provided username and password matches with the one is dictonary
     if(dict.find(username) != dict.end() && dict[username] == password) {
         cout<<"Login Successfully!";
     } else {
