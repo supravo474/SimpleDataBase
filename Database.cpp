9fccb566561c557139e595ccbc4586767745d861
Suprayvo <68844142+supravo474@users.noreply.github.com>
2022-01-17 23:42:41 +0530

    Add files via upload

---
+++ Database.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    // create your dictonary
+    map<string, string>dict = { {"Harry", "TreatPeopleWith"} };
+    string username, password;
+    cout<<"Enter username: "<<endl;
+    cin>>username;
+    cout<<"Enter password: "<<endl;
+    cin>>password;
+    // Check if provided username and password matches with the one is dictonary
+    if(dict.find(username) != dict.end() && dict[username] == password) {
+        cout<<"Login Successfully!";
+    } else {
+        cout<<"Invalid Credentials";
+    }
+    return 0;
+}
